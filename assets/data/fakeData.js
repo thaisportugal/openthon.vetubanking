e5f66e867bc8e364e9175972f9e5b8ed117f9521
Victor Hugo de Souza Pereira <83098581+Lkt226@users.noreply.github.com>
2021-11-14 09:12:38 -0300

    lkt226

---
+++ assets/data/fakeData.js
@@ -0,0 +1,25 @@
+const user = {
+    info: {
+        name: 'John Doe',
+        telefone: ''
+    },
+    cursos: [
+        {
+            nome: 'Curso 1',
+            descricao: 'Descrição do curso 1',
+            porcentagem: '50%', 
+        }
+    ],
+    finances:{
+        valor: 'R$ 100,00',
+        transacoes: [
+            {
+                descricao: 'Transação 1',
+                local: 'Local 1',
+                valor: 'R$ 100,00',
+                data: '01/01/2020',
+                tipo: 'Entrada'	
+            },
+        ]
+    }
+}
